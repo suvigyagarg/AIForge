6bb2272f10c234b06dbe112968ac2ddc58bb0354
suvigyagarg <suvigya.2003.garg@gmail.com>
2025-03-05 22:02:37 +0530

    fix :change the name of app

--- data/Lookup.jsx
+++ data/Lookup.jsx
@@ -1,11 +1,11 @@
 import dedent from "dedent";
 
 export default {
-  SUGGSTIONS: ['Create ToDo App in React', 'Create Budget Track App', 'Create Gym Managment Portal Dashboard', 'Create Quizz App On History', 'Create Login Signup Screen'],
+  SUGGSTIONS: ['Create ToDo App in React', 'Create Budget Track App', 'Create Gym Managment Portal Dashboard', 'Create Quizz App On Pop Culture', 'Create Login Signup Screen'],
   HERO_HEADING: 'What do you want to build?',
   HERO_DESC: 'Prompt, run, edit, and deploy full-stack web apps.',
   INPUT_PLACEHOLDER: 'What you want to build?',
-  SIGNIN_HEADING: 'Continue With Bolt.New 2.0',
+  SIGNIN_HEADING: 'Continue With AIForger',
   SIGNIN_SUBHEADING: 'To use AIForger you must log into an existing account or create one.',
   SIGNIn_AGREEMENT_TEXT: 'By using AIForger, you agree to the collection of usage data for analytics.',
 
